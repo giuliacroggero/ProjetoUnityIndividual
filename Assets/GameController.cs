1ba8a7cf6d80ca6c2abc8955649052a994a2aed0
Giulia <giuliar1@al.insper.edu.br>
2026-04-18 00:40:08 -1000

    Arquivos

---
+++ Assets/GameController.cs
@@ -0,0 +1,19 @@
+public static class GameController
+{
+    private static int collectableCount;
+
+    public static bool gameOver
+    {
+        get { return collectableCount <= 0; }
+    }
+
+    public static void Init()
+    {
+        collectableCount = 4;
+    }
+
+    public static void Collect()
+    {
+        collectableCount--;
+    }
+}
